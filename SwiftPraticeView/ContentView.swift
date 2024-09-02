69d66c8d93422ddde25a777db3d539279b3792aa
soyung sim <simsy1024@gmail.com>
2024-09-02 19:19:21 +0900

    Initial Commit

---
+++ SwiftPraticeView/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftPraticeView
+//
+//  Created by 심소영 on 9/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftPraticeView/SwiftPraticeViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftPraticeViewApp.swift
+//  SwiftPraticeView
+//
+//  Created by 심소영 on 9/2/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftPraticeViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
